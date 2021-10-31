3169316c863362ef495385f61784f305759fd6ef
Peter Boshra <peter.boshra4@gmail.com>
2021-10-31 19:27:42 +0200

    Adding Service Layer

---
+++ src/Service/CompanyService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service;
+
+
+class CompanyService implements CompanyServiceInterface
+{
+    public function create(){
+
+    }
+    public function validate(){
+
+    }
+    public function getAll(){
+
+    }
+}
---
+++ src/Service/CompanyServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Service;
+
+
+interface CompanyServiceInterface
+{
+    public function create();
+    public function validate();
+    public function getAll();
+}
